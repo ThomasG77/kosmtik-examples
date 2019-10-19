9a271af3210b74c31ccabde86339a3ec94b27f10
Thomas Gratier <thomas_gratier@yahoo.fr>
2019-10-20 00:10:59 +0200

    Update stars sample

---
+++ stars/download.sh
@@ -0,0 +1,9 @@
+mkdir data/
+cd data
+wget http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.4.0/cultural/10m-admin-0-countries.zip
+unp *.zip
+rm *.zip
+cd ..
+cd layers/
+wget https://github.com/astronexus/HYG-Database/raw/master/hygxyz.csv
+cd ..
